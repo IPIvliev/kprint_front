6afed5cf2380ed1a4ab0516f9c74d5506c6b4e65
IPIvliev <cccp1945@mail.ru>
2022-08-09 18:55:01 +0300

    Change url for server

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8000/api/';
+const API_URL = 'https://kprint.tech/api/';
 class AuthService {
   login(user) {
     // axios.defaults.xsrfCookieName = 'csrftoken'
